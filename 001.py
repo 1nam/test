329686ae33afac88ea2b96b143f4cef2818f5d7d
zeus <33592162+1nam@users.noreply.github.com>
2019-10-29 23:57:05 -0400

    Update 001.py

--- 001.py
+++ 001.py
@@ -1,1 +1,23 @@
+# []   is an index
+# [0]  adding a number counts letters in phrase
+#  0   python starts counting at zero.
+#  =   phrase becomes the sentence just enter your name
+# ("") can not read code with out proper brackets.
 
+phrase = ("just enter your name")
+# pick letters out of a sentence simply counting 0-19.
+# prints out jay
+print(phrase[0] + phrase[17] + phrase[11] )
+# prints out a
+print(phrase[17] +phrase[17] + phrase[17] )
+# prints out yaj
+print(phrase[11] + phrase[17] + phrase[0] )
+# result read jay down,right,up,left square.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# this one prints out All 19 Letters in the sentance.
+print(phrase[0] + phrase[1] + phrase[2] + phrase[3] + phrase[4]+ phrase[5] + phrase[6]
+      + phrase[7]+ phrase[8] + phrase[9] + phrase[10]+ phrase[11] + phrase[12] + phrase[13]
+     + phrase[14] + phrase[15] + phrase[16] + phrase[17] + phrase[18] + phrase[19])
+# or this one just prints the phrase with out counting. 
+alt = phrase
+print(alt)
